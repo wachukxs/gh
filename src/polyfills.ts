87e98ac4114a003c28b3f727c8d14bd02241e423
wachukxs <nwachukwuossai@gmail.com>
2020-04-01 08:42:19 +0100

    ... better fix

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -70,5 +70,5 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
     An unhandled exception occurred: [BABEL] C:\Users\NWACHUKWU\Documents\angular material\mat-design\dist\mat-design\styles-es2015.js: Could not find plugin "proposal-numeric-separator". Ensure there is an entry in ./available-plugins.js for it. (While processing: "C:\\Users\\NWACHUKWU\\Documents\\angular material\\mat-design\\node_modules\\@babel\\preset-env\\lib\\index.js")
     See "C:\Users\NWACHU~1\AppData\Local\Temp\ng-eFCyIh\angular-errors.log" for further details.
 
-    SOLUTION: https://stackoverflow.com/a/60781705/9259701
+    SOLUTION: https://stackoverflow.com/a/60906538/9259701 > https://stackoverflow.com/a/60781705/9259701
   */
