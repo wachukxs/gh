52209d28bdbe68f93f9fdd6e7503c063bde5b9f8
wachukxs <nwachukwuossai@gmail.com>
2020-07-02 02:08:35 +0100

    hammer ...js

--- src/app/hammer-js.config.ts
+++ src/app/hammer-js.config.ts
@@ -1,10 +1,12 @@
 
 import { HammerGestureConfig } from '@angular/platform-browser';
 import * as hammer from 'hammerjs';
+import { Injectable } from '@angular/core';
 
+@Injectable()
 export class MyHammerConfig extends HammerGestureConfig {
   overrides = {
-    swipe: { direction: hammer.DIRECTION_HORIZONTAL },
+    /* swipe: { direction: hammer.DIRECTION_HORIZONTAL }, */
     pinch: { enable: false },
     rotate: { enable: false }
   };
