65b4cc430d2136556c0274f31dd3f4b2d69e3a95
Ossai Nwachukwu <nwachukwuossai@gmail.com>
2023-01-17 02:28:43 +0300

    tiny updates

--- src/app/image-carousel/image-carousel.component.ts
+++ src/app/image-carousel/image-carousel.component.ts
@@ -6,78 +6,58 @@ import { BaseService } from '../services/base.service';
 import { Observable } from 'rxjs';
 
 // install Swiper modules
-SwiperCore.use([Keyboard, Pagination, Navigation, Virtual, A11y]);
+SwiperCore.use([Keyboard, Pagination, Navigation, Virtual, A11y])
 
 @Component({
-  selector: 'app-image-carousel',
-  templateUrl: './image-carousel.component.html',
-  styleUrls: ['./image-carousel.component.css'],
-  encapsulation: ViewEncapsulation.None,
+    selector: 'app-image-carousel',
+    templateUrl: './image-carousel.component.html',
+    styleUrls: ['./image-carousel.component.css'],
+    encapsulation: ViewEncapsulation.None,
 })
-export class ImageCarouselComponent implements OnInit, AfterViewInit { // is a stepper
-  @ViewChild('swiper', { static: false }) swiper?: SwiperComponent;
-  
-  @ViewChild('rightButton', { static: false, read: ElementRef }) rightButton?: HTMLButtonElement;
-  @ViewChild('leftButton', { static: false, read: ElementRef }) leftButton?: HTMLButtonElement;
-  
-  @Input() displayIndex: null | number = null
+export class ImageCarouselComponent implements OnInit, AfterViewInit {
+    // is a stepper
+    @ViewChild('swiper', { static: false }) swiper?: SwiperComponent
 
-  swiperConfig: SwiperOptions = {
-    // slidesPerView: 3,
-    // spaceBetween: 50,
-    // navigation: true, // automatically added
-    pagination: { clickable: true, dynamicBullets: true },
-    // scrollbar: { draggable: true },
-    // breakpoints: {
-    // }
-  };
+    @ViewChild('rightButton', { static: false, read: ElementRef }) rightButton?: HTMLButtonElement
+    @ViewChild('leftButton', { static: false, read: ElementRef }) leftButton?: HTMLButtonElement
 
-  isSmallScreen$: Observable<boolean> = this.baseService.isSmallScreen$()
+    @Input() displayIndex: null | number = null
 
-  onClick(index: number): void {
-  }
+    swiperConfig: SwiperOptions = {
+        // navigation: false, // automatically added
+        zoom: true,
+        pagination: { clickable: true, dynamicBullets: true },
+    }
 
-  // from hammerjs, not used
-  onSwipeRight(event: any): void {
-  }
-  onSwipeLeft(event: any): void {
-  }
+    isSmallScreen$: Observable<boolean> = this.baseService.isSmallScreen$()
 
-  swipePrev() {
+    onClick(index: number): void {}
 
-  }
+    // from hammerjs, not used
+    onSwipeRight(event: any): void {}
+    onSwipeLeft(event: any): void {}
 
-  swipeNext() {
+    constructor(private baseService: BaseService) {}
 
-  }
+    ngOnInit(): void {
+        // automatically added swiper button navigation
+        this.baseService.isSmallScreen$().subscribe({
+            next: (isSmallScreen) => {
+                console.log('is hand', isSmallScreen)
+                if (isSmallScreen) {
+                    this.swiperConfig.navigation = false
+                } else {
+                    this.swiperConfig.navigation = {
+                        nextEl: '.right-btn-' + this.displayIndex,
+                        prevEl: '.left-btn-' + this.displayIndex,
+                    }
+                }
+            },
+            error: (err) => {
+                console.log('ish', err)
+            },
+        })
+    }
 
-  constructor(private baseService: BaseService) { }
-
-  ngOnInit(): void {
-
-    // automatically added swiper button navigation
-    this.baseService.isSmallScreen$().subscribe({
-      next: (isSmallScreen) => {
-          console.log('is hand', isSmallScreen);
-          if (isSmallScreen) {
-              this.swiperConfig.navigation = false
-          } else {
-            // this.swiperConfig.navigation = true
-
-            this.swiperConfig.navigation = {
-              nextEl: '.right-btn-' + this.displayIndex,
-              prevEl: '.left-btn-' + this.displayIndex,
-            }
-          }  
-      },
-      error: (err) => {
-          console.log('ish', err);
-      }
-  })
-  }
-
-
-  ngAfterViewInit(): void {
-  }
-
+    ngAfterViewInit(): void {}
 }
