abe49e0a821d65756f7b440e79b3b666558b3796
Ossai Nwachukwu <nwachukwuossai@gmail.com>
2023-01-20 20:39:46 +0300

    responsive css styles, custom stepper icons,

--- src/app/create-new-accommodation/create-new-accommodation.component.ts
+++ src/app/create-new-accommodation/create-new-accommodation.component.ts
@@ -1,17 +1,45 @@
+import { StepperOrientation } from '@angular/cdk/stepper';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
+import { Observable } from 'rxjs';
 import { CallerService } from '../services/caller.service';
+import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
 
 @Component({
   selector: 'create-new-accommodation',
   templateUrl: './create-new-accommodation.component.html',
-  styleUrls: ['./create-new-accommodation.component.css']
+  styleUrls: ['./create-new-accommodation.component.css'],
+  providers: [{
+    provide: STEPPER_GLOBAL_OPTIONS, useValue: { displayDefaultIndicatorType: false }
+  }]
 })
 export class CreateNewAccommodationComponent implements OnInit {
 
-  constructor(private createPostDialogRef: MatDialogRef<CreateNewAccommodationComponent>, private callerService: CallerService) { }
+  constructor(private _formBuilder: FormBuilder, private createPostDialogRef: MatDialogRef<CreateNewAccommodationComponent>
+    , private callerService: CallerService) { }
+
+  firstFormGroup = this._formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+  
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+  
+  stepperOrientation: StepperOrientation = 'vertical'
+
 
   ngOnInit(): void {
+    this.callerService.isSmallScreen$().subscribe({
+      next: (value) => { // TODO: do we wanna pipe() instead?
+        if (value) {
+          this.stepperOrientation = 'vertical'
+        } else {
+          this.stepperOrientation = 'horizontal'
+        }
+      }
+    })
   }
 
   postForm: FormData = new FormData()
