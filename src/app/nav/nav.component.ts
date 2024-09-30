636668460eba03c96cc392490936b30554c04c5f
Ossai Nwachukwu <nwachukwuossai@gmail.com>
2024-09-30 08:17:11 +0100

    - added search path to main menu - comment and copy updates.

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core'
+import { Component, OnInit, ViewChild } from '@angular/core'
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout'
 import { Observable } from 'rxjs'
 import { map, shareReplay } from 'rxjs/operators'
@@ -8,13 +8,18 @@ import { CreateNewAccommodationComponent } from '../dialogs/create-new-accommoda
 import { MatDialog } from '@angular/material/dialog'
 import { CreateNewSaleComponent } from '../dialogs/create-new-sale/create-new-sale.component'
 import { AddNewPlaceDialogComponent } from '../dialogs/add-new-place-dialog/add-new-place-dialog.component'
+import { SocketIoChatNamespaceService } from '../services/socket-io.chat-ns.service'
+import { IOEventName } from '../utils/types'
+import { newChatMessage } from '../ngrx-store/actions/corp-member.actions'
+import { AppState } from '../ngrx-store/app.state'
+import { Store } from '@ngrx/store'
 
 @Component({
     selector: 'app-nav',
     templateUrl: './nav.component.html',
     styleUrls: ['./nav.component.css'],
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
     @ViewChild(MatMenuTrigger) trigger: MatMenuTrigger | undefined
 
     isHandset$: Observable<boolean> = this.breakpointObserver
@@ -101,7 +106,22 @@ export class NavComponent {
 
     constructor(
         private breakpointObserver: BreakpointObserver,
+        public store: Store<AppState>,
         private router: Router,
         private dialog: MatDialog,
+        private socketIoChatNamespaceService: SocketIoChatNamespaceService,
     ) {}
+
+    ngOnInit() {
+        /**
+         * NB: The event listeners has to be on the page you want to listen to that event on (or on any component that is rendered).
+         * If it's on a different page (one that isn't rendered), it won't pick it up.
+         * So put the same event listeners on all the page you want to listen to it on.
+         * Not just in one place. It will only get listened to on the current page of the app if it's there.
+         * 
+         * I'm putting it in the nav bar because the nav bar is always visible. (BUT SOMETIMES IT DOESN'T WORK)
+         * Maybe we just need some counter? Of how many unread messages a corper has.
+         */
+        
+    }
 }
